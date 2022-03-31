6bfd4e7c9866bc202bf83b579071f2aa54fc49e3
Bradley-Bird <Bradleybird2@icloud.com>
2022-03-31 12:08:36 -0700

    reduce done

--- reduce.js
+++ reduce.js
@@ -2,9 +2,16 @@
 // OUTPUT: an array of all the sneakers brands
 // REQS: use .reduce
 
-export const getSneaks = (brands) => {};
+export const getSneaks = (brands) => {
+  const shoes = brands.map((brand) => brand.shoes);
+  return shoes.reduce((acc, shoes) => [...acc, ...shoes], []);
+};
 
 // INPUT: the array of dogs from data.js
 // OUTPUT: the average age of the dogs
 // REQS: use .reduce
-export const getAverageAge = (dogs) => {};
+export const getAverageAge = (dogs) => {
+  const dog = dogs.map((dog) => dog.age);
+  const sum = dog.reduce((acc, num) => (acc += num), 0);
+  return sum / dog.length;
+};
